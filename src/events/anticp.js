c1ff7935bc3efc770e60100170e58a90070ab7bd
Angel <angeljohank@gmail.com>
2022-04-28 21:33:52 -0500

    bug fixed

--- src/events/anticp.js
+++ src/events/anticp.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'messageCreate',
 	once: false,
 	async execute(msg) {
-		if (msg.author.bot || !/\bcp\b/i.test(msg.content)) return
+		if (msg.author.bot || !/\b(cp|cepesito)\b/i.test(msg.content)) return
 
 		await msg.reply('deja el cp ctmr :)')
 	},
--- src/events/bcp.js
+++ src/events/bcp.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'messageCreate',
 	once: false,
 	async execute(msg) {
-		if (msg.author.bot || !/\b(bcp|cepesito)\b/i.test(msg.content)) return
+		if (msg.author.bot || !/\b(bcp)\b/i.test(msg.content)) return
 
 		await msg.reply('escuché bcp B)?')
 	},
