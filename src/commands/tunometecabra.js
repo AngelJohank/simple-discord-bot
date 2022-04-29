72ef835c1fea36faf578afa546827b3cfa6024bf
Angel <angeljohank@gmail.com>
2022-04-29 12:30:45 -0500

    added a random baki gif search engine :>

--- src/commands/tunometecabra.js
+++ src/commands/tunometecabra.js
@@ -1,10 +1,23 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
+const fetch = require('node-fetch')
+
+const endpoint =
+	'https://g.tenor.com/v1/search?q=baki&key=R58W60TCG0PF&limit=20'
+
+const getGif = async () => {
+	const index = Math.floor(Math.random() * 21)
+	const response = await fetch(endpoint)
+	const data = await response.json()
+
+	return data.results[index].itemurl
+}
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('tunometecabra')
 		.setDescription('te dice saramambiche'),
 	async execute(interaction) {
-		await interaction.reply('saramambiche 💪')
+		const url = await getGif()
+		await interaction.reply(url)
 	},
 }
