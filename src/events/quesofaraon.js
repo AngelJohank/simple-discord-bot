70fde21a9bfdfd3876a997040df5013e1bdcf9c4
Angel <angeljohank@gmail.com>
2022-05-04 18:58:41 -0500

    bug fixed

--- src/events/quesofaraon.js
+++ src/events/quesofaraon.js
@@ -4,7 +4,7 @@ module.exports = {
 	async execute(msg) {
 		if (msg.author.bot) return
 
-		if (/\b(k|ke|q|qe|que|qué|que)[e]{0,50}\b\??$/m.test(msg.content)) {
+		if (/\b(k|ke|q|qe|que|qué|que)\b$/m.test(msg.content)) {
 			await msg.reply('ricas sobadotas me das <:lipbite:928741545685962853>')
 			return
 		}
